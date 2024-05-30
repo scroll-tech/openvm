3fcd5c17a6bc8396afb138da09a4ec7d48e701d6
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-05-30 17:31:53 -0400

    feat: read pages in arbitrary DAG structure

--- chips/src/lib.rs
+++ chips/src/lib.rs
@@ -1,4 +1,9 @@
+pub mod page1_read;
+pub mod page2_controller;
+pub mod page2_read;
+pub mod page2_requester;
 pub mod page_controller;
+pub mod page_node;
 pub mod page_read;
 /// Chip to range check a value has less than a fixed number of bits
 pub mod range;
@@ -6,3 +11,5 @@ pub mod range_gate;
 pub mod xor_bits;
 pub mod xor_limbs;
 pub mod xor_lookup;
+
+pub const MAX_COMMITMENT_LEN: usize = 8;
