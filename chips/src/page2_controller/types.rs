535303996aacdfcead17a1332bc011b4e11a4db0
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-06-13 11:48:02 -0400

    remove old files

--- chips/src/page2_controller/types.rs
+++
@@ -1,7 +0,0 @@
-use crate::{page1_read::Page1ReadChip, page2_read::Page2ReadChip, page_node::PageNodeReadChip};
-
-pub enum PageNode {
-    PageTerminal(Page1ReadChip), // Assume partition is data | commitment | mult_t | mult_b | mult_p | mult
-    PageBranch(Page2ReadChip), // Assume partition is data | commitment | mult_t | mult_b | mult_p | mult
-    PageMixed(PageNodeReadChip), // Assume partition is data | commitment | mult_t | mult_b | mult_p | mult
-}
