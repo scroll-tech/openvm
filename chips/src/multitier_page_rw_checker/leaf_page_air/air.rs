7a044bb1c2fdd1f8633ba08cda13ff3b6f09a229
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-06-17 16:32:39 -0400

    feat: rename chips to airs

---
+++ chips/src/multitier_page_rw_checker/leaf_page_air/air.rs
@@ -0,0 +1,102 @@
+use afs_stark_backend::air_builders::PartitionedAirBuilder;
+use p3_air::{Air, AirBuilder, AirBuilderWithPublicValues, BaseAir};
+use p3_field::Field;
+use p3_matrix::Matrix;
+
+use super::{
+    columns::{LeafDataCols, LeafPageCols, LeafPageMetadataCols},
+    LeafPageAir, MyPageAir,
+};
+use crate::{
+    is_less_than_tuple::columns::IsLessThanTupleIOCols,
+    sub_chip::{AirConfig, SubAir},
+};
+
+impl<F: Field, const COMMITMENT_LEN: usize> BaseAir<F> for LeafPageAir<COMMITMENT_LEN> {
+    fn width(&self) -> usize {
+        self.air_width()
+    }
+}
+
+impl<const COMMITMENT_LEN: usize> AirConfig for LeafPageAir<COMMITMENT_LEN> {
+    type Cols<T> = LeafPageCols<T>;
+}
+
+impl<
+        AB: AirBuilder + AirBuilderWithPublicValues + PartitionedAirBuilder,
+        const COMMITMENT_LEN: usize,
+    > Air<AB> for LeafPageAir<COMMITMENT_LEN>
+where
+    AB::M: Clone,
+{
+    fn eval(&self, builder: &mut AB) {
+        // only constrain that own_commitment is accurate
+        // partition is physical page data vs metadata
+        let main: &<AB as AirBuilder>::M = &builder.partitioned_main()[1].clone();
+        let local = main.row_slice(0);
+        let pi = builder.public_values().to_vec();
+        let data: &<AB as AirBuilder>::M = &builder.partitioned_main()[0].clone();
+        let cached_data = LeafDataCols::from_slice(&data.row_slice(0), self.idx_len, self.data_len);
+        let next_data = LeafDataCols::from_slice(&data.row_slice(1), self.idx_len, self.data_len);
+        for i in 0..COMMITMENT_LEN {
+            builder.assert_eq(pi[i], local[i]);
+        }
+        builder.assert_one(cached_data.is_leaf);
+        match &self.page_chip {
+            MyPageAir::Initial(i) => {
+                SubAir::eval(i, builder, cached_data.page_cols, ());
+            }
+            MyPageAir::Final(f) => {
+                let metadata = LeafPageMetadataCols::from_slice(
+                    &local,
+                    self.idx_len,
+                    COMMITMENT_LEN,
+                    false,
+                    self.is_less_than_tuple_param.clone(),
+                );
+                let next_aux = LeafPageMetadataCols::from_slice(
+                    &main.row_slice(1),
+                    self.idx_len,
+                    COMMITMENT_LEN,
+                    false,
+                    self.is_less_than_tuple_param.clone(),
+                )
+                .subchip_aux_cols
+                .unwrap()
+                .final_page_aux;
+                let range_inclusion_cols = metadata.range_inclusion_cols.unwrap();
+                let less_than_start = range_inclusion_cols.less_than_start;
+                let greater_than_end = range_inclusion_cols.greater_than_end;
+                builder.assert_zero(
+                    cached_data.page_cols.is_alloc * (less_than_start + greater_than_end),
+                );
+                let subair_aux_cols = metadata.subchip_aux_cols.unwrap();
+                let subairs = self.is_less_than_tuple_air.clone().unwrap();
+                {
+                    let io = IsLessThanTupleIOCols {
+                        x: cached_data.page_cols.idx.clone(),
+                        y: range_inclusion_cols.start.clone(),
+                        tuple_less_than: range_inclusion_cols.less_than_start,
+                    };
+                    let aux = subair_aux_cols.idx_start.clone();
+                    SubAir::eval(&subairs.idx_start, builder, io, aux);
+                }
+                {
+                    let io = IsLessThanTupleIOCols {
+                        x: range_inclusion_cols.end.clone(),
+                        y: cached_data.page_cols.idx.clone(),
+                        tuple_less_than: range_inclusion_cols.greater_than_end,
+                    };
+                    let aux = subair_aux_cols.end_idx.clone();
+                    SubAir::eval(&subairs.end_idx, builder, io, aux);
+                }
+                SubAir::eval(
+                    f,
+                    builder,
+                    [cached_data.page_cols, next_data.page_cols],
+                    next_aux,
+                );
+            }
+        };
+    }
+}
