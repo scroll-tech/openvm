01cd8a80b08a8c7c2c9cf2b56e57266b3c07a62b
Alex Zhao <alexzhao@intrinsictech.com>
2024-07-24 15:48:19 -0400

    docs(vm/mod.rs)

--- vm/src/vm/mod.rs
+++ vm/src/vm/mod.rs
@@ -21,23 +21,41 @@ pub mod config;
 
 pub const DEFAULT_MAX_LEN: usize = (1 << 20) - 100;
 
+/// Parent struct that holds all execution segments, program, config.
+///
+/// Key method is `execute()` which runs the VM. Segment switching is handled by
+/// `ExecutionSegment::stop_execution()`, called every CPU clock cycle, which when `true`
+///  triggers `VirtualMachine::next_segment()`.
+///
+/// Traces are stored in the VM, but chips, traces, and public values should be retrieved using
+/// `get_chips()`, `get_traces()`, and `get_pis()` respectively.
 pub struct VirtualMachine<const WORD_SIZE: usize, F: PrimeField32> {
     pub config: VmConfig,
     pub program: Vec<Instruction<F>>,
     pub segments: Vec<Box<ExecutionSegment<WORD_SIZE, F>>>,
     pub traces: Vec<DenseMatrix<F>>,
+
     // NOT PUBLIC by design, adjust only for testing
     max_len: usize,
 }
 
+/// Struct that holds the current state of the VM. For now, includes memory, input stream, and hint stream.
+/// Hint stream cannot be added to during execution, but must be copied because it is popped from.
 pub struct VirtualMachineState<F: PrimeField32> {
+    /// Current state of the CPU
     state: CpuState,
+    /// Current memory of the CPU
     memory: HashMap<(F, F), F>,
+    /// Input stream of the CPU
     input_stream: VecDeque<Vec<F>>,
+    /// Hint stream of the CPU
     hint_stream: VecDeque<F>,
 }
 
 impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
+    /// Create a new VM with a given config, program, and input stream.
+    ///
+    /// The VM will start with a single segment, which is created from the initial state of the CPU.
     pub fn new(config: VmConfig, program: Vec<Instruction<F>>, input_stream: Vec<Vec<F>>) -> Self {
         let mut vm = Self {
             config,
@@ -55,11 +73,15 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
         vm
     }
 
+    /// Sets the max length of the VM. This should only be used for testing purposes.
     pub fn set_test_segments(&mut self, max_len: usize) {
         self.max_len = max_len;
         self.segments[0].set_test_segments(max_len);
     }
 
+    /// Create a new segment with a given state.
+    ///
+    /// The segment will be created from the given state and the program.
     pub fn new_segment(&mut self, state: VirtualMachineState<F>) {
         let program = self.program.clone();
         let mut segment = ExecutionSegment::new(self, program, state);
@@ -69,6 +91,7 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
         self.segments.push(Box::new(segment));
     }
 
+    /// Retrieves the current state of the VM by querying the last segment.
     pub fn get_state(&self) -> VirtualMachineState<F> {
         VirtualMachineState {
             state: self.segments.last().unwrap().cpu_chip.get_state(),
@@ -78,20 +101,26 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
         }
     }
 
+    /// Create a new segment with the current state of the VM.
+    ///
+    /// This is done by querying the last segment's state and creating a new segment from it.
     pub fn next_segment(&mut self) {
         let state = self.get_state();
         self.new_segment(state);
     }
 
+    /// Retrieves the CPU options from the VM's config.
     pub fn options(&self) -> CpuOptions {
         self.config.cpu_options()
     }
 
+    /// Executes the VM by calling `ExecutionSegment::generate_traces()` until the CPU hits `TERMINATE`
+    /// and `cpu_chip.is_done`. Between every segment, the VM will call `generate_commitments()` and then
+    /// `next_segment()`.
     pub fn execute(&mut self) -> Result<(), ExecutionError> {
         let mut result = vec![];
         loop {
             result.extend(self.segments.last_mut().unwrap().generate_traces()?);
-            // Add additional traces for committing
             result.extend(self.segments.last_mut().unwrap().generate_commitments()?);
             if self.segments.last_mut().unwrap().cpu_chip.is_done {
                 break;
@@ -102,6 +131,7 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
         Ok(())
     }
 
+    /// Retrieves the non-empty traces from the VM.
     pub fn get_traces(&self) -> Vec<DenseMatrix<F>> {
         self.traces
             .clone()
@@ -110,6 +140,7 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
             .collect()
     }
 
+    /// Retrieves the maximum log degree of the VM's trace.
     pub fn max_log_degree(&self) -> Result<usize, ExecutionError> {
         let mut checker_trace_degree = 0;
         for trace in &self.traces {
@@ -118,6 +149,7 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
         Ok(log2_strict_usize(checker_trace_degree))
     }
 
+    /// Retrieves the public inputs from the VM's segments, filtering by nonempty traces.
     pub fn get_pis(&self) -> Vec<Vec<F>> {
         let initial: Vec<Vec<F>> = self
             .segments
@@ -134,7 +166,8 @@ impl<const WORD_SIZE: usize, F: PrimeField32> VirtualMachine<WORD_SIZE, F> {
     }
 }
 
-// TODO: make into struct method
+/// Retrieves all chips from the VM's segments, filtering by nonempty traces. Cannot be made into
+/// struct method because of generics.
 pub fn get_all_chips<const WORD_SIZE: usize, SC: StarkGenericConfig>(
     vm: &VirtualMachine<WORD_SIZE, Val<SC>>,
 ) -> Vec<&dyn AnyRap<SC>>
