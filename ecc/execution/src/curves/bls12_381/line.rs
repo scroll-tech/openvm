03ad9264eb2cb874d08a95dc844b3dbbf1647902
Yu Jiang Tham <yoojbruin@gmail.com>
2024-10-01 18:49:12 -0400

    Update bls12-381 for (w^{-2}x, w^{-3}y) twist

--- ecc/execution/src/curves/bls12_381/line.rs
+++ ecc/execution/src/curves/bls12_381/line.rs
@@ -14,13 +14,13 @@ where
     [b, c]
 }
 
-/// Multiplies two elements in 013 form and outputs the product in 01234 form
-pub fn mul_013_by_013<Fp, Fp2>(
+/// Multiplies two elements in 013 form and outputs the product in 012345 form
+pub fn mul_023_by_023<Fp, Fp2>(
     line_0: [Fp2; 2],
     line_1: [Fp2; 2],
     // TODO[yj]: once this function is moved into a chip, we can use the xi property instead of passing in this argument
     xi: Fp2,
-) -> [Fp2; 5]
+) -> [Fp2; 6]
 where
     Fp: Field,
     Fp2: FieldExtension<2, BaseField = Fp>,
@@ -31,34 +31,36 @@ where
     let c1 = line_1[1];
 
     // where w⁶ = xi
-    // l0 * l1 = 1 + (b0 + b1)w + (b0b1)w² + (c0 + c1)w³ + (b0c1 + b1c0)w⁴ + (c0c1)w⁶
-    //         = (1 + c0c1 * xi) + (b0 + b1)w + (b0b1)w² + (c0 + c1)w³ + (b0c1 + b1c0)w⁴
-    let l0 = Fp2::ONE + c0 * c1 * xi;
-    let l1 = b0 + b1;
-    let l2 = b0 * b1;
-    let l3 = c0 + c1;
-    let l4 = b0 * c1 + b1 * c0;
+    // l0 * l1 = b0b1 + (b0c1 + b1c0)w² + (b0 + b1)w³ + (c0c1)w⁵ + w⁶
+    //         = (b0b1 + xi) + (b0c1 + b1c0)w² + (b0 + b1)w³ + (c0c1)w⁵
+    let l0 = b0 + b1 + xi;
+    let l1 = Fp2::ZERO;
+    let l2 = b0 * c1 + b1 * c0;
+    let l3 = b0 + b1;
+    let l4 = Fp2::ZERO;
+    let l5 = c0 * c1;
 
-    [l0, l1, l2, l3, l4]
+    [l0, l1, l2, l3, l4, l5]
 }
 
-pub fn mul_by_013<Fp, Fp2, Fp12>(f: Fp12, line: [Fp2; 2]) -> Fp12
+pub fn mul_by_023<Fp, Fp2, Fp12>(f: Fp12, line: [Fp2; 2]) -> Fp12
 where
     Fp: Field,
     Fp2: FieldExtension<2, BaseField = Fp>,
     Fp12: FieldExtension<6, BaseField = Fp2>,
 {
-    mul_by_01234(f, [Fp2::ONE, line[0], Fp2::ZERO, line[1], Fp2::ZERO])
+    mul_by_012345(
+        f,
+        [line[0], Fp2::ZERO, line[1], Fp2::ONE, Fp2::ZERO, Fp2::ZERO],
+    )
 }
 
-pub fn mul_by_01234<Fp, Fp2, Fp12>(f: Fp12, x: [Fp2; 5]) -> Fp12
+pub fn mul_by_012345<Fp, Fp2, Fp12>(f: Fp12, x: [Fp2; 6]) -> Fp12
 where
     Fp: Field,
     Fp2: FieldExtension<2, BaseField = Fp>,
     Fp12: FieldExtension<6, BaseField = Fp2>,
 {
-    let mut x_extend: [Fp2; 6] = [Fp2::ZERO; 6];
-    x_extend[..5].clone_from_slice(&x);
-    let x_fp12 = Fp12::from_coeffs(x_extend);
+    let x_fp12 = Fp12::from_coeffs(x);
     f * x_fp12
 }
