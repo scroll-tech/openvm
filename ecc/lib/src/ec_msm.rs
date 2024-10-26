1b405403f5a64550bbfc8c1dd668af2c4e39b5a2
Andrew Gu <67293785+MonkeyKing-1@users.noreply.github.com>
2024-10-26 14:45:22 -0400

    feat: use msm_serial variant

--- ecc/lib/src/ec_msm.rs
+++ ecc/lib/src/ec_msm.rs
@@ -1,8 +1,17 @@
-use elliptic_curve::group::cofactor::CofactorCurveAffine;
+use std::ops::Neg;
+
+use elliptic_curve::{group::cofactor::CofactorCurveAffine, Group};
+use ff::{
+    derive::bitvec::{order::Lsb0, view::AsBits},
+    PrimeField,
+};
 use itertools::Itertools;
-use snark_verifier_sdk::snark_verifier::halo2_base::{
-    halo2_proofs::halo2curves::secp256k1::{Fq, Secp256k1Affine},
-    utils::{log2_ceil, ScalarField},
+use snark_verifier_sdk::snark_verifier::{
+    halo2_base::{
+        halo2_proofs::halo2curves::secp256k1::{Fq, Secp256k1Affine},
+        utils::log2_ceil,
+    },
+    util::arithmetic::CurveAffine,
 };
 
 // Note that the sqrt is at most 16, so this is a very dumb implementation.
@@ -16,6 +25,12 @@ pub fn sqrt_ceil(n: usize) -> usize {
     16
 }
 
+pub fn msm_axvm(g: Vec<Secp256k1Affine>, scalars: Vec<Fq>) -> Secp256k1Affine {
+    let mut acc = Secp256k1Affine::identity();
+    msm_serial::<Secp256k1Affine>(&scalars, &g, &mut acc);
+    acc
+}
+
 pub fn multi_scalar_multiply(g: Vec<Secp256k1Affine>, scalars: Vec<Fq>) -> Secp256k1Affine {
     let n = g.len();
     let lambda = 256;
@@ -23,23 +38,188 @@ pub fn multi_scalar_multiply(g: Vec<Secp256k1Affine>, scalars: Vec<Fq>) -> Secp2
     let t = (lambda + s - 1) / s;
     let b = log2_ceil(t as u64) - log2_ceil(log2_ceil(t as u64) as u64);
     println!("s: {}, t: {}, b: {}", s, t, b);
-    let e = scalars.iter().map(|s| scalar_to_bits(*s)).collect_vec();
+    let e = scalars
+        .iter()
+        .map(|s| scalar_to_bits::<Secp256k1Affine>(*s))
+        .collect_vec();
     let (g, e) = multi_scalar_multiply_internal0(g, e, s, t, lambda);
     let g = multi_scalar_multiply_internal1(g, e, b);
     multi_scalar_multiply_internal2(g, s)
 }
 
-pub fn scalar_to_bits(scalar: Fq) -> Vec<bool> {
-    let scalar = scalar.to_bytes_le();
-    let mut bits = Vec::with_capacity(256);
+// pub fn scalar_to_bits(scalar: Fq) -> Vec<bool> {
+//     let scalar = scalar.to_bytes_le();
+//     let mut bits = Vec::with_capacity(256);
+//     for b in scalar {
+//         let mut i = b;
+//         for _ in 0..8 {
+//             bits.push(i & 1 == 1);
+//             i /= 2;
+//         }
+//     }
+//     bits
+// }
+
+pub fn scalar_to_bits<C: CurveAffine>(scalar: C::Scalar) -> Vec<bool> {
+    let scalar = scalar.to_repr();
+    let scalar = scalar.as_bits::<Lsb0>();
+    let mut bits = Vec::with_capacity(C::Scalar::NUM_BITS as usize);
     for b in scalar {
-        let mut i = b;
-        for _ in 0..8 {
-            bits.push(i & 1 == 1);
-            i /= 2;
-        }
+        // let mut i = b;
+        // for _ in 0..8 {
+        //     bits.push(i & 1 == 1);
+        //     i /= 2;
+        // }
+        bits.push(*b);
     }
     bits
+}
+
+pub fn scalar_to_bytes<C: CurveAffine>(scalar: C::Scalar) -> Vec<u8> {
+    let scalar = scalar_to_bits::<C>(scalar);
+    let mut bytes = vec![0; C::Scalar::NUM_BITS.div_ceil(8u32) as usize];
+    for (i, b) in scalar.iter().enumerate() {
+        if *b {
+            bytes[i / 8] |= 1 << (i % 8);
+        }
+    }
+    bytes
+}
+
+/// Performs a multi-scalar multiplication operation.
+///
+/// This function will panic if coeffs and bases have a different length.
+pub fn msm_serial<C: CurveAffine>(coeffs: &[C::Scalar], bases: &[C], acc: &mut C) {
+    let coeffs: Vec<_> = coeffs.iter().map(|a| scalar_to_bytes::<C>(*a)).collect();
+
+    let c = if bases.len() < 4 {
+        1
+    } else if bases.len() < 32 {
+        3
+    } else {
+        (f64::from(bases.len() as u32)).ln().ceil() as usize
+    };
+
+    let field_byte_size = C::Scalar::NUM_BITS.div_ceil(8u32) as usize;
+    // OR all coefficients in order to make a mask to figure out the maximum number of bytes used
+    // among all coefficients.
+    let mut acc_or = vec![0; field_byte_size];
+    for coeff in &coeffs {
+        for (acc_limb, limb) in acc_or.iter_mut().zip(coeff.iter()) {
+            *acc_limb |= *limb;
+        }
+    }
+    let max_byte_size = field_byte_size
+        - acc_or
+            .iter()
+            .rev()
+            .position(|v| *v != 0)
+            .unwrap_or(field_byte_size);
+    if max_byte_size == 0 {
+        return;
+    }
+    let number_of_windows = max_byte_size * 8 / c + 1;
+
+    for current_window in (0..number_of_windows).rev() {
+        for _ in 0..c {
+            *acc = ec_add::<C>(*acc, *acc);
+        }
+
+        #[derive(Clone, Copy)]
+        enum Bucket<C: CurveAffine> {
+            None,
+            Affine(C),
+            // Projective(C::Curve),
+        }
+
+        impl<C: CurveAffine> Bucket<C> {
+            fn add_assign(&mut self, other: &C) {
+                *self = match *self {
+                    Bucket::None => Bucket::Affine(*other),
+                    Bucket::Affine(a) => Bucket::Affine(ec_add(a, *other)),
+                    // Bucket::Projective(mut a) => {
+                    //     a += *other;
+                    //     Bucket::Projective(a)
+                    // }
+                }
+            }
+
+            fn add(self, mut other: C) -> C {
+                match self {
+                    Bucket::None => other,
+                    Bucket::Affine(a) => {
+                        other = ec_add(a, other);
+                        other
+                    } // Bucket::Projective(a) => other + a,
+                }
+            }
+        }
+
+        let mut buckets: Vec<Bucket<C>> = vec![Bucket::None; 1 << (c - 1)];
+
+        for (coeff, base) in coeffs.iter().zip(bases.iter()) {
+            let coeff = get_booth_index(current_window, c, coeff.as_ref());
+            if coeff.is_positive() {
+                buckets[coeff as usize - 1].add_assign(base);
+            }
+            if coeff.is_negative() {
+                buckets[coeff.unsigned_abs() as usize - 1].add_assign(&base.neg());
+            }
+        }
+
+        // Summation by parts
+        // e.g. 3a + 2b + 1c = a +
+        //                    (a) + b +
+        //                    ((a) + b) + c
+        let mut running_sum = C::Curve::identity().into();
+        for exp in buckets.into_iter().rev() {
+            running_sum = exp.add(running_sum);
+            *acc = ec_add(*acc, running_sum);
+        }
+    }
+}
+
+fn get_booth_index(window_index: usize, window_size: usize, el: &[u8]) -> i32 {
+    // Booth encoding:
+    // * step by `window` size
+    // * slice by size of `window + 1``
+    // * each window overlap by 1 bit * append a zero bit to the least significant end
+    // Indexing rule for example window size 3 where we slice by 4 bits:
+    // `[0, +1, +1, +2, +2, +3, +3, +4, -4, -3, -3 -2, -2, -1, -1, 0]``
+    // So we can reduce the bucket size without preprocessing scalars
+    // and remembering them as in classic signed digit encoding
+
+    let skip_bits = (window_index * window_size).saturating_sub(1);
+    let skip_bytes = skip_bits / 8;
+
+    // fill into a u32
+    let mut v: [u8; 4] = [0; 4];
+    for (dst, src) in v.iter_mut().zip(el.iter().skip(skip_bytes)) {
+        *dst = *src
+    }
+    let mut tmp = u32::from_le_bytes(v);
+
+    // pad with one 0 if slicing the least significant window
+    if window_index == 0 {
+        tmp <<= 1;
+    }
+
+    // remove further bits
+    tmp >>= skip_bits - (skip_bytes * 8);
+    // apply the booth window
+    tmp &= (1 << (window_size + 1)) - 1;
+
+    let sign = tmp & (1 << window_size) == 0;
+
+    // div ceil by 2
+    tmp = (tmp + 1) >> 1;
+
+    // find the booth action index
+    if sign {
+        tmp as i32
+    } else {
+        ((!(tmp - 1) & ((1 << window_size) - 1)) as i32).neg()
+    }
 }
 
 fn multi_scalar_multiply_internal0(
@@ -141,7 +321,7 @@ fn multi_scalar_multiply_internal2(g: Vec<Secp256k1Affine>, s: usize) -> Secp256
     acc
 }
 
-pub fn ec_add(g: Secp256k1Affine, h: Secp256k1Affine) -> Secp256k1Affine {
+pub fn ec_add<C: CurveAffine>(g: C, h: C) -> C {
     let r = g + h;
     r.into()
 }
--- ecc/lib/src/tests/ec_msm.rs
+++ ecc/lib/src/tests/ec_msm.rs
@@ -6,13 +6,16 @@ use num_bigint_dig::BigUint;
 use rand::Rng;
 use snark_verifier_sdk::snark_verifier::{
     halo2_base::{
-        halo2_proofs::halo2curves::secp256k1::{Fp, Fq, Secp256k1Affine},
+        halo2_proofs::halo2curves::{
+            msm::multiexp_serial,
+            secp256k1::{Fp, Fq, Secp256k1Affine},
+        },
         utils::ScalarField,
     },
     util::arithmetic::CurveAffine,
 };
 
-use crate::ec_msm::multi_scalar_multiply;
+use crate::ec_msm::msm_axvm;
 
 pub fn get_base() -> Secp256k1Affine {
     let base = (
@@ -32,13 +35,13 @@ pub fn get_base() -> Secp256k1Affine {
     .unwrap()
 }
 
-pub fn base_line(g: Vec<Secp256k1Affine>, scalars: Vec<Fq>) -> Secp256k1Affine {
-    let mut res = Secp256k1Affine::identity();
-    for (g, s) in g.iter().zip(scalars.iter()) {
-        res = (res + g.mul(*s)).into();
-    }
-    res
-}
+// pub fn base_line(g: Vec<Secp256k1Affine>, scalars: Vec<Fq>) -> Secp256k1Affine {
+//     let mut res = Secp256k1Affine::identity();
+//     for (g, s) in g.iter().zip(scalars.iter()) {
+//         res = (res + g.mul(*s)).into();
+//     }
+//     res
+// }
 
 #[test]
 pub fn msm_simple_test() {
@@ -49,7 +52,7 @@ pub fn msm_simple_test() {
     let base2 = base.mul(a).into();
     let g = vec![base, base2];
     let scalars = vec![b, one];
-    let res = multi_scalar_multiply(g, scalars);
+    let res = msm_axvm(g, scalars);
     assert_eq!(res, base.mul(a + b).into());
 }
 
@@ -67,7 +70,7 @@ pub fn msm_rand_test(n: usize, disable_base_line: bool) {
         .map(|_| Fq::from(rng.gen_range(0..100)))
         .collect::<Vec<_>>();
     let time = std::time::Instant::now();
-    let res = multi_scalar_multiply(g.clone(), scalar_muls.clone());
+    let res = msm_axvm(g.clone(), scalar_muls.clone());
     println!("MSM Time Taken: {:?}", time.elapsed());
     let expected = if disable_base_line {
         let scalar = base_muls
@@ -77,10 +80,15 @@ pub fn msm_rand_test(n: usize, disable_base_line: bool) {
             .fold(Fq::from(0), |acc, x| acc + x);
         base.mul(scalar).into()
     } else {
+        // let time = std::time::Instant::now();
+        // let expected = base_line(g, scalar_muls);
+        // println!("Base Line Time Taken: {:?}", time.elapsed());
+        // expected
         let time = std::time::Instant::now();
-        let expected = base_line(g, scalar_muls);
+        let mut acc = Secp256k1Affine::identity().into();
+        multiexp_serial(&scalar_muls, &g, &mut acc);
         println!("Base Line Time Taken: {:?}", time.elapsed());
-        expected
+        acc.into()
     };
     assert_eq!(res, expected);
 }
@@ -97,5 +105,5 @@ pub fn msm_rand_test_1000() {
 
 #[test]
 pub fn msm_rand_test_10000() {
-    msm_rand_test(10000, true);
+    msm_rand_test(10000, false);
 }
