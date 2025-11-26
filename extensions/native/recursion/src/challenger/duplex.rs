49be4ba8ae545677c8d98c991ea8bce29a1c617b
kunxian xia <xiakunxian130@gmail.com>
2025-11-26 14:20:42 +0800

    clippy

--- extensions/native/recursion/src/challenger/duplex.rs
+++ extensions/native/recursion/src/challenger/duplex.rs
@@ -80,7 +80,7 @@ impl<C: Config> DuplexChallengerVariable<C> {
     // Observes multiple elements from an array.
     // This is equivalent to calling `observe` multiple times, but more efficient.
     pub fn observe_slice_opt(&self, builder: &mut Builder<C>, arr: &Array<C, Felt<C::F>>) {
-        let next_pos = builder.poseidon2_multi_observe(&self.sponge_state, self.input_ptr, &arr);
+        let next_pos = builder.poseidon2_multi_observe(&self.sponge_state, self.input_ptr, arr);
 
         builder.assign(&self.input_ptr, self.io_empty_ptr + next_pos.clone());
         builder.if_ne(next_pos, Usize::from(0)).then_or_else(
