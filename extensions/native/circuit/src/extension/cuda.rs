217d1e05b12e2507a48f045d6f4bd0036a48d0fd
Ray Gao <qg2153@columbia.edu>
2026-03-23 22:30:58 -0400

    fix

--- extensions/native/circuit/src/extension/cuda.rs
+++ extensions/native/circuit/src/extension/cuda.rs
@@ -98,19 +98,6 @@ impl VmProverExtension<GpuBabyBearPoseidon2Engine, DenseRecordArena, Native>
         );
         inventory.add_executor_chip(poseidon2);
 
-        let hint_air: &HintSpaceProviderAir = inventory.next_air::<HintSpaceProviderAir>()?;
-        let cpu_range_checker = range_checker
-            .cpu_chip
-            .clone()
-            .expect("VariableRangeCheckerChipGPU is expected to be hybrid with cpu_chip");
-        let cpu_chip = Arc::new(HintSpaceProviderChip::new(
-            hint_air.hint_bus,
-            cpu_range_checker,
-            timestamp_max_bits,
-        ));
-        let provider_gpu = HintSpaceProviderChipGpu::new(cpu_chip.clone());
-        inventory.add_periphery_chip(provider_gpu);
-
         inventory.next_air::<NativeSumcheckAir>()?;
         let sumcheck =
             NativeSumcheckChipGpu::new(range_checker.clone(), timestamp_max_bits, cpu_chip);
